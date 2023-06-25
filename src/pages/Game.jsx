c1d1668fa64b064c33634f2f7b83bfdf3ff4643d
Jaci-Xavier <devxvr@gmail.com>
2023-06-25 19:51:15 -0300

    fix: regras do jogo

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -10,56 +10,41 @@ function Game() {
   const [shuffledDeck, setShuffledDeck] = useState([]);
   const [playerCards, setPlayerCards] = useState([]);
   const [computerCards, setComputerCards] = useState([]);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
 
-  const shuffleDeck = () => {
-    const deckCopy = deck.slice();
+  const shuffleDeck = (d) => {
+    const deckCopy = d.slice();
     const magicNumber = 0.5;
     deckCopy.sort(() => Math.random() - magicNumber);
-    setShuffledDeck(deckCopy);
+    return deckCopy;
+  };
+
+  const addCardToComputer = () => {
+    const newPlayerScore = playerCards.reduce((acc, curr) => acc + curr.value, 0);
+    const newComputerScore = computerCards.reduce((acc, curr) => acc + curr.value, 0);
+    const magicNumber = 21;
+
+    if (newComputerScore < magicNumber && newComputerScore <= newPlayerScore) {
+      const computer = shuffledDeck.shift();
+      setComputerCards((prevComputerCards) => [...prevComputerCards, computer]);
+    }
   };
 
   const hitCardForPlayer = () => {
     const player = shuffledDeck.shift();
-    setPlayerCards((prevPlayerCards) => [...prevPlayerCards, player]);
+    setPlayerCards((prevPlayerCards) => {
+      const newPlayerCards = [...prevPlayerCards, player];
+      console.log(newPlayerCards);
+      addCardToComputer();
+      console.log(computerCards);
+      return newPlayerCards;
+    });
   };
 
-  const checkWinner = () => {
-    const playerScore = playerCards.reduce((acc, curr) => acc + curr.value, 0);
-    const computerScore = computerCards.reduce((acc, curr) => acc + curr.value, 0);
-    const magicNumber = 21;
-
-    let result;
-    if (playerScore > magicNumber) {
-      result = 'Computer';
-    } else if (computerScore > magicNumber) {
-      result = 'Player';
-    } else if (playerScore > computerScore) {
-      result = 'Player';
-    } else if (computerScore > playerScore) {
-      result = 'Computer';
-    } else {
-      result = 'Draw';
-    }
-
-    if (result === 'Player') {
-      setWins((prevWins) => prevWins + 1);
-    } else if (result === 'Computer') {
-      setLosses((prevLosses) => prevLosses + 1);
-    }
-
-    console.log(`The winner is ${result}`);
-    return result;
-  };
-
-  const stand = () => {
-    const computer = shuffledDeck.slice(0, 1);
-    setShuffledDeck(shuffledDeck.slice(1));
-    setComputerCards((prevComputerCards) => [...prevComputerCards, ...computer]);
-    checkWinner();
-  };
-
-  const modifyCardValues = () => {
-    const modifiedDeck = deck.map((card) => {
+  const sumCards = (player, computer) => {
+    const modifiedPlayerCards = player.map((card) => {
       if (card.value === 'king' || card.value === 'queen' || card.value === 'jack') {
         return { ...card, value: 10 };
       } if (card.value === 'ace') {
@@ -68,39 +53,96 @@ function Game() {
       return card;
     });
 
-    setShuffledDeck(modifiedDeck);
+    const modifiedComputerCards = computer.map((card) => {
+      if (card.value === 'king' || card.value === 'queen' || card.value === 'jack') {
+        return { ...card, value: 10 };
+      } if (card.value === 'ace') {
+        return { ...card, value: 11 };
+      }
+      return card;
+    });
+
+    const pS = modifiedPlayerCards.reduce((acc, curr) => acc + curr.value, 0);
+    const cs = modifiedComputerCards
+      .reduce((acc, curr) => acc + curr.value, 0);
+
+    setPlayerScore(pS);
+    setComputerScore(cs);
+  };
+
+  const checkWinner = () => {
+    const magicNumber = 21;
+    sumCards(playerCards, computerCards);
+
+    if (playerScore > magicNumber) {
+      console.log('Player busts');
+      setLosses((prevLosses) => prevLosses + 1);
+      return 'Computer';
+    }
+
+    if (computerScore > magicNumber) {
+      console.log('Computer busts');
+      setWins((prevWins) => prevWins + 1);
+      return 'Player';
+    }
+
+    if (playerCards.length === 2 && playerScore === magicNumber) {
+      console.log('Player has Blackjack');
+      setWins((prevWins) => prevWins + 1);
+      return 'Player';
+    }
+
+    if (computerCards.length === 2 && computerScore === magicNumber) {
+      console.log('Computer has Blackjack');
+      setLosses((prevLosses) => prevLosses + 1);
+      return 'Computer';
+    }
+
+    if (playerScore > computerScore) {
+      console.log('Player wins');
+      setWins((prevWins) => prevWins + 1);
+      return 'Player';
+    }
+
+    if (computerScore > playerScore) {
+      console.log('Computer wins');
+      setLosses((prevLosses) => prevLosses + 1);
+      return 'Computer';
+    }
+
+    console.log('Draw');
+    return 'Draw';
   };
 
   const playGame = () => {
-    modifyCardValues();
-    shuffleDeck();
+    setShuffledDeck(shuffleDeck(deck));
+    setGameStarted(true);
     console.log('Jogo iniciado');
   };
 
+  const stand = () => {
+    setGameStarted(false);
+    checkWinner();
+  };
+
   useEffect(() => {
-    const initialPlayerCards = () => {
+    if (shuffledDeck.length > 0 && gameStarted) {
       const player = shuffledDeck.splice(0, 2);
       setPlayerCards(player);
       console.log('Cartas do jogador', player);
-    };
 
-    const initialComputerCards = () => {
       const computer = shuffledDeck.splice(0, 2);
       setComputerCards(computer);
       console.log('Cartas do computador', computer);
-    };
-
-    if (shuffledDeck.length > 0) {
-      initialPlayerCards();
-      initialComputerCards();
     }
-  }, [shuffledDeck]);
+  }, [shuffledDeck, gameStarted]);
 
   return (
     <div>
       <Header />
       <section>
         <div>
+          <h1>Player</h1>
           {playerCards.map((card, index) => (
             <div key={ index }>
               <div>
@@ -110,17 +152,16 @@ function Game() {
           ))}
         </div>
         <div>
+          <h1>Computador</h1>
           {computerCards.map((card, index) => (
             <div key={ index }>
-              {index === 0 ? (
-                <div>
+              <div>
+                {index === 0 && gameStarted ? (
                   <img src={ hiddenCard } alt="Carta do Computador" />
-                </div>
-              ) : (
-                <div>
+                ) : (
                   <img src={ card.image } alt="Carta do Computador" />
-                </div>
-              )}
+                )}
+              </div>
             </div>
           ))}
         </div>
